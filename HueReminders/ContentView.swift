0edccba70933dfda43b4919fbc2f86260ce92ac4
Jan <jsvensson51@yahoo.com>
2020-07-08 08:42:54 +0200

    Initial Commit

---
+++ HueReminders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HueReminders
+//
+//  Created by Jan Svensson on 2020-07-08.
+//  Copyright © 2020 Jan Svensson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
